5115ba6ff3273f058342b0c0fb7eb177c4ec9176
Akash Nelson <89798657+Akashew@users.noreply.github.com>
2025-07-25 14:23:55 -0500

    Add files via upload

---
+++ backend/src/main/java/com/geosight/backend/GeoSightbackendApplication.java
@@ -0,0 +1,13 @@
+package com.geosight.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoSightbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoSightbackendApplication.class, args);
+	}
+
+}
