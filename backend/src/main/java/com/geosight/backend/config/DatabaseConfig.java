30f0ef94829568070ab0da46dc5c5551ffac3698
Akashew <akashnelson00@gmail.com>
2025-07-25 15:35:49 -0500

    initialized Spring Boot app

--- backend/src/main/java/com/geosight/backend/config/DatabaseConfig.java
+++ backend/src/main/java/com/geosight/backend/config/DatabaseConfig.java
@@ -1,1 +1,28 @@
+package com.geosight.backend.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        Dotenv dotenv = Dotenv.configure()
+                              .directory("./backend") // Adjust path if needed
+                              .ignoreIfMalformed()
+                              .ignoreIfMissing()
+                              .load();
+
+        return DataSourceBuilder.create()
+                .url(dotenv.get("DB_URL"))
+                .username(dotenv.get("DB_USERNAME"))
+                .password(dotenv.get("DB_PASSWORD"))
+                .build();
+    }
+}
 
