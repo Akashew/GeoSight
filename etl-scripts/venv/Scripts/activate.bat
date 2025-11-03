5c18f9d659110ecf28463517583a8b28bb79c4d8
Akashew <akashnelson00@gmail.com>
2025-11-02 18:34:35 -0600

    rework

--- etl-scripts/venv/Scripts/activate.bat
+++ etl-scripts/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\akash\GeoSight\etl-scripts\venv
+set VIRTUAL_ENV=C:\Users\akash\OneDrive - University Of Houston\Documents\GeoSight\etl-scripts\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
